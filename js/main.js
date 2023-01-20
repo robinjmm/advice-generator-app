58b6d7e6bc2ef1a6cf4e8c0255aebc02e4df1468
J.M. Robin <johnmichaelrobin@gmail.com>
2023-01-21 03:44:15 +0800

    Import stylesheets in the main js file

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
+import "@styles/styles.scss";
+import "@fontawesome/fontawesome.min.css";
+import "@fontawesome/brands.min.css";
+
 const button = document.querySelector(".js-button");
 const adviceText = document.querySelector(".js-card__advice");
 const adviceNum = document.querySelector(".js-card__advice-num")
 
 button.addEventListener("click", async (event) => {
-	const response = await fetch("https://api.adviceslip.com/advice");
-	const json = await response.json();
-	const {id, advice} = json.slip;
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const {slip} = await response.json();
+    const {id, advice} = slip;
 
-	adviceText.innerText = advice;
-	adviceNum.innerText = id;
+    adviceText.innerText = advice;
+    adviceNum.innerText = id;
 });
